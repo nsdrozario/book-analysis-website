915189e552a1ff70b44c737adb08eda9fdedc9cd
Nathaniel D'Rozario <nsdrozario@gmail.com>
2020-12-16 16:55:06 -0800

    navbar style update

--- getanchor.js
+++ getanchor.js
@@ -1,23 +1,20 @@
 let anchors = ["section2", "writing", "audience", "author"];
+let selected = 0;
 $(window).on('scroll', function() {
-   
-    for (let i = 0; i < anchors.length; i++) {
-
-
-
-        if ($(window).scrollTop() > $("#"+anchors[i]).offset().top  ) {
-            
-            for (let j = 0; j < anchors.length; j++) {
-
-                $("#"+anchors[j]+"-link").removeClass("active-text-highlight");
-
-            }
-
-            $("#"+anchors[i]+"-link").addClass("active-text-highlight");
-
-
-        } 
-
+    let i;
+    for (i = anchors.length-1; i >= 0; i--) {
+        if ($(window).scrollTop() > $("#"+anchors[i]).offset().top) {
+            break;
+        }
     }
-
+    
+    if (i != -1) {
+        let j;
+        for (j = 0; j < anchors.length; j++) {
+            $("#"+anchors[j]+"-link").parent().removeClass("active-text-highlight");
+        }
+        $("#"+anchors[i]+"-link").parent().addClass("active-text-highlight");
+    } else {
+        $("#section2-link").parent().addClass("active-text-highlight");
+    }
 })
