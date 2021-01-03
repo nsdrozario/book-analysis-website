d8e6695a07398805fdbdc1ca0a3b8b70834d7fca
Nathaniel D'Rozario <nsdrozario@gmail.com>
2021-01-02 21:48:38 -0800

    delete get_img_credits.js

--- get_img_credits.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $.get("img/IMAGE_LICENSE.html", function(data, status) {
-        $("#image-credits-area").html(
-            "<p>" +
-            data +
-            "</p>"
-        )
-    })
-});
